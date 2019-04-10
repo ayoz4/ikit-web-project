6abc2f56ed6ba3a1a822b1f68f95173052d39e1e
Lab3 <32977377+ayoz4@users.noreply.github.com>
2019-04-11 00:48:03 +0700

    addElement adde

--- WebSite/9/9.js
+++ WebSite/9/9.js
@@ -1,6 +1,6 @@
 function deleteElement (currentName) {
     var elements = document.getElementById('main').getElementsByClassName('child');
-    console.log(elements);
+    var emptyDiv = document.createElement('h1').innerHTML = 'Список пуст!';
 
     for (let i = 0; i < elements.length; i++) {
 
@@ -8,5 +8,24 @@ function deleteElement (currentName) {
             elements[i].remove();
             break;
         }
+
+        if (elements.length == 0) {
+            main.appendChild(emptyDiv);
+            console.log('hi');
+        }
     }
-}   
+}
+
+function addElement () {
+    var element = document.createElement('div');
+    var classNameAttr = document.createAttribute('class');
+    classNameAttr.value = 'child';
+    element.setAttributeNode(classNameAttr);
+    element.innerHTML = '<button>delete</button>';
+    main.appendChild(element);
+}
+
+function checkElementsArray () {
+    var elements = document.getElementById('main').getElementsByClassName('child');
+    console.log(elements);
+}
