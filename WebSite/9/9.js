21f5673ce91331663cdffe2384dde1e59dcab883
Lab3 <32977377+ayoz4@users.noreply.github.com>
2019-04-11 01:11:20 +0700

    addElement done

--- WebSite/9/9.js
+++ WebSite/9/9.js
@@ -2,6 +2,8 @@ function deleteElement (currentName) {
     var elements = document.getElementById('main').getElementsByClassName('child');
     var emptyDiv = document.createElement('h1').innerHTML = 'Список пуст!';
 
+    console.log(currentName)
+
     for (let i = 0; i < elements.length; i++) {
 
         if (elements[i].getAttribute('name') == currentName) {
@@ -19,8 +21,14 @@ function deleteElement (currentName) {
 function addElement () {
     var element = document.createElement('div');
     var classNameAttr = document.createAttribute('class');
+    var nameAttr = document.createAttribute('name');
+    var onClickAttr = document.createAttribute('onclick');
+    onClickAttr.value = `deleteElement(${upTheCounter() + 1})`;
     classNameAttr.value = 'child';
+    nameAttr.value = `${upTheCounter()}`;
     element.setAttributeNode(classNameAttr);
+    element.setAttributeNode(onClickAttr);
+    element.setAttributeNode(nameAttr);
     element.innerHTML = '<button>delete</button>';
     main.appendChild(element);
 }
@@ -28,4 +36,10 @@ function addElement () {
 function checkElementsArray () {
     var elements = document.getElementById('main').getElementsByClassName('child');
     console.log(elements);
+}
+
+var counter = 3;
+
+function upTheCounter () {
+    return counter = counter + 1;
 }
