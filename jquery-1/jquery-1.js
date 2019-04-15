35fa68fbd5597cc33972ca48453f3189c748c99a
Roman <ayoz4@yandex.ru>
2019-04-15 20:15:15 +0700

    data collect done

--- jquery-1/jquery-1.js
+++ jquery-1/jquery-1.js
@@ -1,6 +1,43 @@
+/* eslint-disable consistent-return */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-alert */
+/* eslint-disable func-names */
 /* eslint-disable no-undef */
-$(document).ready(() => {
-  $('#id').click(() => {
-    $('#id').wrap('<h1>Hello world!</h1>');
-  });
+
+$('#name').keyup(function () {
+  if ($(this).val() === '') {
+    $('#pass, #confirmPass').attr('disabled', 'disabled');
+  } else {
+    $('#pass, #confirmPass').removeAttr('disabled');
+  }
+});
+
+$('form').submit(function () {
+  const pass = $('#pass').val();
+  const confirmPass = $('#confirmPass').val();
+
+  if (pass !== confirmPass) {
+    alert('Passwords do not match');
+    return false;
+  }
+
+  if ($('#name').val() === '') {
+    alert('Please, enter the name');
+    return false;
+  }
+  if ($('#pass').val() === '') {
+    alert('Please, enter the password');
+    return false;
+  }
+
+  if ($('#selector').val() === 'Choose role') {
+    alert('Please, choose role');
+    return false;
+  }
+
+  console.log($(this).serializeArray());
+
+  $.post($(this).serializeArray());
+
+  alert('Successfully!');
 });
